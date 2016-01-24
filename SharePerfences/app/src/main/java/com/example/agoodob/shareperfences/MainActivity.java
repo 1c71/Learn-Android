fe6c64bb808f222544b1d253994ebe365d09ce19
1c7 <1003211008@qq.com>
2016-01-24 16:46:43 +0800

    asd

--- SharePerfences/app/src/main/java/com/example/agoodob/shareperfences/MainActivity.java
+++ SharePerfences/app/src/main/java/com/example/agoodob/shareperfences/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.agoodob.shareperfences;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String PREFES_FILES = "com.teamtreehouse.sharepreferencesapp.preferences";
+    private SharedPreferences.Editor mEditor;
+    private SharedPreferences mSharedPreferences;
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditText = (EditText) findViewById(R.id.editText);
+        mSharedPreferences = getSharedPreferences(PREFES_FILES, Context.MODE_PRIVATE);
+        // 参数是 key value
+        mEditText = mSharedPreferences.edit();
+
     }
 }
