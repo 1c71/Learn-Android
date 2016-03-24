cfeedb6d9476266e945b19365425db14c053154a
1c7 <1003211008@qq.com>
2016-03-24 22:32:27 +0800

    Udacity Sunshine App Lession 3 done

---
+++ CrashCourse/app/src/main/java/com/example/agoodob/crashcourse/SeriesActivity.java
@@ -0,0 +1,16 @@
+package com.example.agoodob.crashcourse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 系列
+ */
+public class SeriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_series);
+    }
+}
